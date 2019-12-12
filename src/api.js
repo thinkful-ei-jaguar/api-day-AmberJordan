78130a268dfc01b04952e6167b8ab8093ff2e27e
amberdoo11 <57081110+amberdoo11@users.noreply.github.com>
2019-12-12 10:18:48 -0700

    part 2 of api module

---
+++ src/api.js
@@ -0,0 +1,8 @@
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[amber-jordan]';
+const getItems = function(){
+    return Promise.resolve('A successful response!');
+};
+export default {
+    getItems
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+  .then(res => console.log(res));
+
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
