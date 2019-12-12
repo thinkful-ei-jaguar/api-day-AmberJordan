b2d30034a4521588bb5547cf4669b68565e1c943
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-12 13:29:43 -0800

    number 8 is done

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -82,7 +82,6 @@ const handleEditShoppingItemSubmit = function () {
     event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
     const itemName = $(event.currentTarget).find('.shopping-item').val();
-    store.findAndUpdateName(id, itemName);
     render();
   });
 };
@@ -90,7 +89,6 @@ const handleEditShoppingItemSubmit = function () {
 const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const id = getItemIdFromElement(event.currentTarget);
-    store.findAndToggleChecked(id);
     render();
   });
 };
